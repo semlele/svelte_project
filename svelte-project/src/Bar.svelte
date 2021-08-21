5ec2eb8f1f0d4ae85398c019428039ba95d0f7f3
semlele <ken99suzu@icloud.com>
2021-08-21 16:09:43 +0900

    svelte_tutorial_vol.1

---
+++ svelte-project/src/Bar.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let label = "";
+    export let value = 0;
+</script>
+
+<div class="container">
+    <div class="label">{label}</div>
+    <div class="bar" style="--value: {value}px"></div>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        align-items: center;
+        height: 1.5em;
+        padding: 0.2em 0;
+    }
+
+    .label {
+        margin-right: 0.5em;
+        width: 4em;
+        text-align: right;
+    }
+
+    .bar {
+        background-color: white;
+        height: 100%;
+        width: var(--value, 0);
+    }
+</style>
---
+++ svelte-project/src/Pokemon.svelte
@@ -0,0 +1,54 @@
+<script>
+    import Bar from "./Bar.svelte";
+
+    export let pokemon;
+
+    const typeToClass = {
+        'くさ':  'grass',
+        'ほのお': 'fire',
+        'みず': 'water'
+    }
+
+    let open = false;
+    const toggle = () => open = !open;
+</script>
+
+<div class="pokemon {typeToClass[pokemon.type]}" on:click={toggle}>
+    <h2>{pokemon.name}</h2>
+    {#if open}
+        <div class="stats">
+            <h3>ステータス</h3>
+            <Bar label="HP" value={pokemon.hp} />
+            <Bar label="攻撃" value={pokemon.atk} />
+            <Bar label="防御" value={pokemon.def} />
+            <Bar label="特攻" value={pokemon.sAtk} />
+            <Bar label="特防" value={pokemon.sDef} />
+            <Bar label="素早" value={pokemon.spd} />
+        </div>
+    {:else}
+        <div>クリックで詳細を見る</div>
+    {/if}
+</div>
+
+<style>
+    h2, h3 {font-weight: normal;}
+
+    .pokemon {
+        color: white;
+        padding: 1em;
+        cursor: pointer;
+    }
+
+    .grass {
+        background-color: rgb(96, 184, 69);
+    }
+
+    .fire {
+        background-color: rgb(226, 82, 82);
+    }
+
+    .water {
+        background-color: rgb(62, 104, 240);
+    }
+</style>
+
